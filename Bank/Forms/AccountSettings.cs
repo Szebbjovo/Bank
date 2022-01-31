4a940427034f219ee3e36cc90972063f3bb4ef22
Ferko Moisi <varazsferenc@gmail.com>
2022-01-31 12:44:39 +0200

    Hello, van torles

--- Bank/Forms/AccountSettings.cs
+++ Bank/Forms/AccountSettings.cs
@@ -16,5 +16,24 @@ namespace Bank.Forms
         {
             InitializeComponent();
         }
+
+        //ACCOUNT DELETE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BankDataContext context = new BankDataContext();
+            //a helper static classeltudom kivel van dolgom es megkeresem :)))
+            var cont = context.Posts.Where(actil => actil.username == usernameLoginHelper.usernameHp).FirstOrDefault();
+
+
+            //linq-el torlom 
+            var posts = from p in context.Posts
+                        where p.ID == cont.ID
+                        select p;
+            foreach (var post in posts) {
+                context.Posts.Remove(post);
+            }
+            context.SaveChanges();
+            
+        }
     }
 }
